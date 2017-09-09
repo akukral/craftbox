e591a9701b3c771976929cd3ec5d0495cf76ed1c
Allan Kukral <allan@vectorliving.com>
2017-09-09 13:04:30 -0500

    updating sw.js

--- html/sw.js
+++ html/sw.js
@@ -1,55 +1,55 @@
-//This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
+// This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
 
-//Install stage sets up the offline page in the cahche and opens a new cache
-self.addEventListener('install', function(event) {
-  event.waitUntil(preLoad());
-});
-
-var preLoad = function(){
+var preLoad = function() {
   console.log('[PWA Builder] Install Event processing');
   return caches.open('pwabuilder-offline').then(function(cache) {
     console.log('[PWA Builder] Cached index and offline page during Install');
     return cache.addAll(['/offline.html', '/index.php']);
   });
-}
+};
 
-self.addEventListener('fetch', function(event) {
-  console.log('The service worker is serving the asset.');
-  event.respondWith(checkResponse(event.request).catch(function() {
-    return returnFromCache(event.request)}
-  ));
-  event.waitUntil(addToCache(event.request));
-});
-
-var checkResponse = function(request){
+var checkResponse = function(request) {
   return new Promise(function(fulfill, reject) {
-    fetch(request).then(function(response){
-      if(response.status !== 404) {
-        fulfill(response)
+    fetch(request).then(function(response) {
+      if (response.status !== 404) {
+        fulfill(response);
       } else {
-        reject()
+        reject();
       }
-    }, reject)
+    }, reject);
   });
 };
 
-var addToCache = function(request){
-  return caches.open('pwabuilder-offline').then(function (cache) {
-    return fetch(request).then(function (response) {
-      console.log('[PWA Builder] add page to offline'+response.url)
+var addToCache = function(request) {
+  return caches.open('pwabuilder-offline').then(function(cache) {
+    return fetch(request).then(function(response) {
+      console.log('[PWA Builder] add page to offline' + response.url)
       return cache.put(request, response);
     });
   });
 };
 
-var returnFromCache = function(request){
-  return caches.open('pwabuilder-offline').then(function (cache) {
-    return cache.match(request).then(function (matching) {
-     if(!matching || matching.status == 404) {
-       return cache.match('offline.html')
-     } else {
-       return matching
-     }
+var returnFromCache = function(request) {
+  return caches.open('pwabuilder-offline').then(function(cache) {
+    return cache.match(request).then(function(matching) {
+      if (!matching || matching.status == 404) {
+        return cache.match('offline.html');
+      } else {
+        return matching;
+      }
     });
   });
 };
+
+// Install stage sets up the offline page in the cache and opens a new cache
+self.addEventListener('install', function(event) {
+  event.waitUntil(preLoad());
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log('The service worker is serving the asset.');
+  event.respondWith(checkResponse(event.request).catch(function() {
+    return returnFromCache(event.request);
+  }));
+  event.waitUntil(addToCache(event.request));
+});
